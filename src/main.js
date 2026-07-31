248692a026b46987d7490faa687d0f22bc60221e
Semhar Tes <semhartes7@gmail.com>
2026-07-31 15:27:02 -0400

    Add styling

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import { register, setNotFound, startRouter } from './router.js';
-import { leaguesView } from './views/leagues.js';
+import "./style.css";
+import { register, setNotFound, startRouter } from "./router.js";
+import { leaguesView } from "./views/leagues.js";
 import { standingsView } from "./views/standings.js";
 import { teamView } from "./views/team.js";
 
-register('/leagues', leaguesView);
+register("/leagues", leaguesView);
 register("/standings", standingsView);
-register('/teams', teamView);   // search mode
-register('/team', teamView);    // detail mode (?id=...)
+register("/teams", teamView); // search mode
+register("/team", teamView); // detail mode (?id=...)
 
 setNotFound((params, el) => {
-  el.innerHTML = '<h1>Not found</h1>';
+  el.innerHTML = "<h1>Not found</h1>";
 });
 
-startRouter();
+startRouter(); 
